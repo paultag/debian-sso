61f4998531164edba2a8de37e988059acd78145f
Martin Zobel-Helas <zobel@debian.org>
2014-04-01 19:56:40 +0200

    add id
    Signed-off-by: Martin Zobel-Helas <zobel@debian.org>

--- debsso/views.py
+++ debsso/views.py
@@ -4,5 +4,5 @@ from oauth2_provider.views.generic import ProtectedResourceView
 
 class ApiEndpoint(ProtectedResourceView):
     def get(self, request, *args, **kwargs):
-        resp = {'kind': 'debsso#personOpenIdConnect', 'gender': 'male', 'sub': 'zobel', 'name': 'Martin Zobel-Helas', 'given_name': 'Martin', 'family_name': 'Zobel-Helas', 'email': 'zobel@debian.org', 'email_verified': 'true'}
+        resp = {'id': 'zobel', 'kind': 'debsso#personOpenIdConnect', 'gender': 'male', 'sub': 'zobel', 'name': 'Martin Zobel-Helas', 'given_name': 'Martin', 'family_name': 'Zobel-Helas', 'email': 'zobel@debian.org', 'email_verified': 'true'}
         return HttpResponse(json.dumps(resp), content_type="application/json" )
